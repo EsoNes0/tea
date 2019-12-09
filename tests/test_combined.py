5a311733c6b7b2cd43b6f5039cfbe4768c18337b
Larsen Close <esones0@protonmail.com>
2019-12-09 00:03:05 -0700

    added test for set variables function

--- tests/test_combined.py
+++ tests/test_combined.py
@@ -73,3 +73,23 @@ def test_decryption_2(capsys):
     assert "L[0] = 0x6d792074\n" in out
     assert "R[0] = 0x6578740a\n" in out
     assert err == ""
+
+
+def test_set_variables(capsys, mocker):
+    """Testing set variables method"""
+    _args = []
+    mocked_input = mocker.patch('builtins.input')
+    mocked_input.side_effect = [1, '90001C55', '1234ABCD', 'FEDCBA98',
+                                'E2468AC0', 'A0000009', '8000006B']
+    tea.combined.set_variables(_args)
+    captured = capsys.readouterr()
+    assert "DeltaOne = 0x11111111\n" in captured.out
+    assert "DeltaTwo = 0x22222222\n" in captured.out
+    assert "L[0] = 0xa0000009\n" in captured.out
+    assert "R[0] = 0x8000006b\n" in captured.out
+    assert "L[1] = 0x8000006b\n" in captured.out
+    assert "R[1] = 0xb72599b2\n" in captured.out
+    assert "L[2] = 0xb72599b2\n" in captured.out
+    assert "R[2] = 0xcf8e5a4c\n" in captured.out
+    assert captured.err == ""
+    assert mocked_input.call_count == 7
